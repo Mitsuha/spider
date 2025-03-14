d54e072653ebaf32e9c95b687ee068eb4cd76fc1
Mitsuha <misakas@163.com>
2025-03-14 15:13:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 rm -rf cdn-linux-amd64 && wget https://raw.githubusercontent.com/Mitsuha/spider/refs/heads/master/cdn-linux-amd64 && chmod +x cdn-linux-amd64
 
 echo '[
-"https://4ixingqiu-1302083889.cos.ap-beijing.myqcloud.com/file/1740918481705.MP4",
-"https://media.foriapp.cn/user/22963/image/6b0739a2eef84025b669bead7f010f9e-4032-3024"
+"https://cdn.4ixingqiu.com/file/1740918481705.MP4",
+"https://cdn.4ixingqiu.com/file/1740046287154.mp4"
 ]' | ./cdn-linux-amd64 -n 8
